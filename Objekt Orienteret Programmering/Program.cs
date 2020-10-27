6dab1f5b6465e666099e22921c1ea1eee1a7dcb7
josefine jacobsen <josejaco@live.dk>
2020-10-27 10:18:32 +0100

    Add project files.

---
+++ Objekt Orienteret Programmering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objekt_Orienteret_Programmering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
